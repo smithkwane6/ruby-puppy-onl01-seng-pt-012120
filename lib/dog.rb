0cd7a9ec60a0a1a3a17613266de8da9f3a9be685
Kwane Smith <smithkwane6@gmail.com>
2020-02-11 21:00:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,6 +5,23 @@ class Dog
 
   def initialize(name)
     @name = name
+    self.save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear_all
+    @@all = []
+  end
+
+  def self.print_all
+    @@all.each do |dog|
+      puts dogs.name
+    end
+
+  def save
     @@all << self
   end
 end
